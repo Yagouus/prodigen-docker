026a046f1bd9782020c2c2037d357ac7cd86ebe4
Yago Fontenla Seco <yagofontenla95@gmail.com>
2017-07-26 11:59:56 +0200

    updated script

--- launch.sh
+++ launch.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get install -y python docker-ce python-pip
 sudo pip install docker
 wget https://github.com/docker/compose/releases/download/1.14.0/docker-compose-`uname -s`-`uname -m` -O docker-compose
