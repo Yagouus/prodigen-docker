5322ddd1e3e4a98c4f678233a400d9f77a6ecb3d
Rodrigo Martínez <dev@brunneis.com>
2017-10-02 17:09:36 +0200

    Various fixes

--- launch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get install -y python docker-ce python-pip
-sudo pip install docker
-wget https://github.com/docker/compose/releases/download/1.14.0/docker-compose-`uname -s`-`uname -m` -O docker-compose
-sudo chmod u+x docker-compose
-./docker-compose up -d
